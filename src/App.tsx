f90b438aa6852dbfe207575bab216b114637ce24
Bruno Jurakić <178019478+brunojurakic@users.noreply.github.com>
2025-08-05 20:37:46 +0200

    add react to vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Hello, React + Electron Forge + Vite + TypeScript!</h1>
+      <p>test</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/renderer.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+if (container) {
+  createRoot(container).render(<App />);
+} else {
+  console.error('Root element not found');
+}
