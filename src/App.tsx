1f2e63b61133597696f54726a30613bfbb4479d6
Bruno Jurakić <178019478+brunojurakic@users.noreply.github.com>
2025-08-05 21:17:18 +0200

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 const App: React.FC = () => {
   return (
     <div style={{ padding: 32 }}>
-      <h1>Hello, React + Electron Forge + Vite + TypeScript!</h1>
+      <h1 className='text-purple-500'>Hello, React + Electron Forge + Vite + TypeScript!</h1>
       <p>test</p>
+      <Button variant="outline">shadcn/ui Button</Button>
     </div>
   );
 };
